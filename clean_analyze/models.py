7698f48dbebf6b5c3e5f6da7eab4900ba40bba96
kessa99 <kipre325@gmail.com>
2025-12-11 18:42:46 +0000

    update

--- clean_analyze/models.py
+++ clean_analyze/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+from django.utils import timezone
+
+class Dataset(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nom_fichier = models.CharField(max_length=255)
+    fichier_csv = models.FileField(upload_to='datasets/')
+    data_upload = models.DateTimeField(default=timezone.now)
+    stats_csv = models.FileField(upload_to='stats/', null=True, blank=True)
+    notes_numpy = models.FileField(upload_to='notes_numpy/', null=True, blank=True)
